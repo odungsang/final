3ef5f267f37d7e714e1e4b3bb501d9e79f74e2d4
Leesh <odungsang@gmail.com>
2023-06-15 13:31:46 +0900

    기능 구현 및 스타일 적용 완료(동작확인)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState("Purple");
+
+  const handleColor = (event) => {
+    const { value } = event.target;
+    
+    const regExp = new RegExp("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
+    const isHex = regExp.test(value);
+    
+    const newColor = isHex ? value : value;
+    
+    setColor(newColor);
+  };
+
   return (
-    <div className="App">
-      
+    <div className="container" style={{ backgroundColor: color }}>
+      <p>Background Color : {color} </p>
+      <input type="text" onChange={handleColor} />
     </div>
   );
-}
+};
 
 export default App;
